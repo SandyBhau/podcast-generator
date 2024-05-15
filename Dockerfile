c28b18a93519c8e7eb63b35633cdd317028188b7
DataScience-Sandesh <117732968+SandyBhau@users.noreply.github.com>
2024-05-15 16:18:20 +0000

    Update

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM ubuntu:latest
+
+# Install dependencies
+
+RUN apt-get update && apt-get install -y \
+    python3.10 \ 
+    python3-pip \
+    git
+
+RUN pip3 install PyYAML
+
+COPY feed.py /usr/bin/feed.py
+
+COPY entrypoint.sh /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "==================="
+
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${INPUT_EMAIL}"
+git config --global --add safe.directory /github/workspace
+
+python3 /usr/bin/feed.py
+
+git add -a && git commit -m "Update Feed"
+
+git push --set-upstream origin main
+
+echo "==================="
