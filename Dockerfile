5712ae9093dece9adedc19cdd27cd681df93bf54
DataScience-Sandesh <117732968+SandyBhau@users.noreply.github.com>
2024-05-15 16:30:24 +0000

    Update Files

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM ubuntu:latest
 
-# Install dependencies
-
 RUN apt-get update && apt-get install -y \
     python3.10 \ 
     python3-pip \
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,9 @@ git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
-python3 /usr/bin/feed.py
+python3 /urs/bin/feed.py
 
-git add -a && git commit -m "Update Feed"
+git add -A && git commit -m "Update Feed"
 
 git push --set-upstream origin main
 
