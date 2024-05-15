f6ad5a44796a33f88e150526e771e1742234489b
DataScience-Sandesh <117732968+SandyBhau@users.noreply.github.com>
2024-05-15 17:36:56 +0000

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,6 @@ git config --global --add safe.directory /github/workspace
 python3 /usr/bin/feed.py
 
 git add -A && git commit -m "Update Feed"
-
 git push --set-upstream origin main
 
 echo "==================="
