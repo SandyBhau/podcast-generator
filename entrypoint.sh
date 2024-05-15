dbd59437f51526bce0232966424513825c968273
DataScience-Sandesh <117732968+SandyBhau@users.noreply.github.com>
2024-05-15 16:31:16 +0000

    Val

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
-python3 /urs/bin/feed.py
+python3 /usr/bin/feed.py
 
 git add -A && git commit -m "Update Feed"
 
